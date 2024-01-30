4ec1babb2c7e43abfd3ebcabdd7974b906aceedb
div-vik <shivamambast5@gmail.com>
2024-01-30 13:51:54 +0530

    Add child component

--- app/page.js
+++ app/page.js
@@ -1,5 +1,16 @@
 import Image from "next/image";
+import Hero from "./ui/Home/Hero";
+import Topic from "./ui/Home/Topic";
+import Feedback from "./ui/Home/Feedback";
+import Features from "./ui/Home/Features";
 
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <Hero />
+      <Topic />
+      <Feedback />
+      <Features />
+    </div>
+  );
 }
