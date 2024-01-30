ac078ccb0d912d2de0be400aefa6eb1a9ac32ce1
div-vik <shivamambast5@gmail.com>
2024-01-30 17:03:58 +0530

    Add join component

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Hero from "./ui/Home/Hero";
 import Topic from "./ui/Home/Topic";
 import Feedback from "./ui/Home/Feedback";
 import Features from "./ui/Home/Features";
+import Contact from "./ui/Home/Contact";
 
 export default function Home() {
   return (
@@ -11,6 +12,18 @@ export default function Home() {
       <Topic />
       <Feedback />
       <Features />
+      <Contact />
+
+      <div className="px-[42px] mt-10 flex flex-col justify-center items-center">
+        <p className="text-center  text-[16.331px] font-bold">
+          We are always ready to partner up with ambitious and passionate
+          coaches. Join us in revolutionising life skills as a trainer…
+        </p>
+
+        <button className="w-[159.152px] h-[52px] rounded-3xl bg-[#E53990] mt-[35px] text-center text-[15.758px] font-bold lowercase">
+          explore courses
+        </button>
+      </div>
     </div>
   );
 }
