b69321d78a71c2b30bd0522bd9572e9d0b11716d
div-vik <shivamambast5@gmail.com>
2024-01-30 16:05:25 +0530

    Add features

--- app/ui/Home/data.js
+++ app/ui/Home/data.js
@@ -77,3 +77,75 @@ export const feedbacks = [
     designation: "College student",
   },
 ];
+
+export const checks = [
+  {
+    title: "1-on-1 Guidance",
+  },
+  {
+    title: "Highly Personalised Instruction",
+  },
+  {
+    title: "Network of Experts & Driven Community",
+  },
+  {
+    title: "Objective-Based Learning Interface",
+  },
+  {
+    title: "Instant Help For You",
+  },
+  {
+    title: "Urgent Tutoring",
+  },
+  {
+    title: "24/7 AI Chat Support",
+  },
+  {
+    title: "Lifetime Access",
+  },
+  {
+    title: "Access to resources for a lifetime",
+  },
+  {
+    title: "No-Limit to Learning",
+  },
+  {
+    title: "Live additional discussions",
+  },
+  {
+    title: "Group Discussions",
+  },
+  {
+    title: "Updated & On-Demand Classes",
+  },
+  {
+    title: "Open Ended Projects & Assessments for Practice",
+  },
+  {
+    title: "Weekly Workshops",
+  },
+  {
+    title: "Downloadable Certificates",
+  },
+  {
+    title: "Customizable support at every stage",
+  },
+];
+
+export const features = [
+  {
+    title: "Leadership",
+  },
+  {
+    title: "Productivity",
+  },
+  {
+    title: "Communication",
+  },
+  {
+    title: "Career",
+  },
+  {
+    title: "Effective Teamwork",
+  },
+];
