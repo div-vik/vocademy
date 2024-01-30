69a27af4ef9d46f88c940059615ebcec7112e08d
div-vik <shivamambast5@gmail.com>
2024-01-30 13:51:20 +0530

    Add constant data

---
+++ app/ui/Home/data.js
@@ -0,0 +1,79 @@
+export const topics = [
+  {
+    icon: "/yoga.svg",
+    title: "Yoga",
+  },
+  {
+    icon: "/personality.svg",
+    title: "Personality Development",
+  },
+  {
+    icon: "/cooking.svg",
+    title: "Cooking Lessons",
+  },
+  {
+    icon: "/coding.svg",
+    title: "Coding",
+  },
+  //   {
+  //     icon: "/yoga.svg",
+  //     title: "Yoga",
+  //   },
+  //   {
+  //     icon: "/yoga.svg",
+  //     title: "Yoga",
+  //   },
+  //   {
+  //     icon: "/yoga.svg",
+  //     title: "Yoga",
+  //   },
+  //   {
+  //     icon: "/yoga.svg",
+  //     title: "Yoga",
+  //   },
+  //   {
+  //     icon: "/yoga.svg",
+  //     title: "Yoga",
+  //   },
+  //   {
+  //     icon: "/yoga.svg",
+  //     title: "Yoga",
+  //   },
+  //   {
+  //     icon: "/yoga.svg",
+  //     title: "Yoga",
+  //   },
+  //   {
+  //     icon: "/yoga.svg",
+  //     title: "Yoga",
+  //   },
+  //   {
+  //     icon: "/yoga.svg",
+  //     title: "Yoga",
+  //   },
+  //   {
+  //     icon: "/yoga.svg",
+  //     title: "Yoga",
+  //   },
+];
+
+export const feedbacks = [
+  {
+    name: "- Krish Parihar",
+    review:
+      "I am so glad to be trained in life skills by Vocademy. I have learned to cope with adolescent issues and balance my academics simultaneously. I have seen so many changes.",
+    designation: "College student",
+  },
+  {
+    name: "- Krish Parihar",
+    review:
+      "I am so glad to be trained in life skills by Vocademy. I have learned to cope with adolescent issues and balance my academics simultaneously. I have seen so many changes.",
+    designation: "College student",
+  },
+  {
+    name: "- Krish Parihar",
+    review:
+      "I am so glad to be trained in life skills by Vocademy. I have learned to cope with adolescent issues and balance my academics simultaneously. I have seen so many changes.",
+    designation: "College student",
+  },
+];
