185fcc48d1846f830f215f0e6bef90e42db6327c
div-vik <shivamambast5@gmail.com>
2024-02-03 00:55:49 +0530

    Add accordian component

--- app/ui/Home/Faq.jsx
+++ app/ui/Home/Faq.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { faqs } from "./data";
 import Image from "next/image";
-import { MdArrowDownward } from "react-icons/md";
+import Accordian from "./Accordian";
 
 const Faq = () => {
   return (
@@ -10,17 +10,7 @@ const Faq = () => {
 
       <div className="flex flex-col w-full px-[43px] lg:px-[62px] xl:px-[192px] mt-[46px] lg:mt-[54px] xl:mt-[24px]">
         {faqs.map((faq) => (
-          <div
-            key={faq.question}
-            className="flex justify-between gap-[47px]  items-center mb-[9px]"
-          >
-            <p className="text-[10.4px] lg:text-[21.081px] xl:text-[24px] font-medium">
-              {faq.question}
-            </p>
-            <div className="p-[1px] rounded-full bg-[#FF77A1] flex items-center">
-              <MdArrowDownward className="w-[12.529px] lg:w-[24.594px] h-[12.529px] xl:w-[28px] lg:h-[24.594px] xl:h-[28px]" />
-            </div>
-          </div>
+          <Accordian key={faq.question} faq={faq} />
         ))}
       </div>
     </div>
