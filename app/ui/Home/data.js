c5dfbeb494194fbbc28264110bd83d75b044ae09
div-vik <shivamambast5@gmail.com>
2024-02-03 00:55:23 +0530

    Add answers

--- app/ui/Home/data.js
+++ app/ui/Home/data.js
@@ -174,48 +174,90 @@ export const features = [
 
 export const faqs = [
   {
+    answer:
+      "These are practical skills that help one to excel in a specific job or trade.",
+    points: [
+      "Spellings",
+      "Phonics",
+      "Foundation Courses",
+      "Communication Skills",
+      "Tailoring",
+      "Yoda",
+      "Personality Development",
+      "Cooking Lesson",
+      "Coding",
+      "Financial Literacy",
+      "Career Counselling",
+      "Public Speaking",
+      "Creative Writing",
+      "Basic Computer Skills",
+    ],
     question: "What is vocational training?",
   },
   {
+    answer:
+      "Click on the button at the top right corner of the home page to create an account.",
     question: "How do I sign up for an account on Vocademyy?",
   },
   {
+    answer:
+      "Vocademy offers 15 different life-skill based courses ranging from financial literacy to Academic foundation courses. All the details are given on the home page.",
     question: "What courses does Voademyy offer?",
   },
   {
+    answer: "The fee range is Rs 2000-3000/  for  5 hours of each course.",
     question: "What is the fee and duration of the courses?",
   },
   {
+    answer:
+      "Your certificate shall be made downloadable once you have successfully completed your training program.",
     question: "How do I receive my certificate after completing my course?",
   },
   {
+    answer: "No, currently there is no refund policy at LearnVoc.",
     question: "Is there a refund policy available?",
   },
   {
+    answer:
+      "At Vocademy, we help you connect with people who share the same interest and passion via group chats for 24*7 communication with peers. The trainer shall provide individual support and doubt clearance directly after the sessions on appointment.",
     question: "How does Vocademyy ensure a supportive learning environment?",
   },
   {
+    answer:
+      "Yes, the trainer’s guidance and the networks that you build during your training programs will bring you further opportunities.",
     question: "Are there further opportunities after completion of a course?",
   },
   {
+    answer:
+      "Yes, at Vocademy, after you have paid for your course you get lifetime access to it.",
     question: "Do I have lifetime access to the resources?",
   },
   {
+    answer: "No, additional charges. There is only one time payment.",
     question: "Are there any additional charges?",
   },
   {
+    answer:
+      "You can book a trial for a particular training program. However, no free course is available.",
     question: "Are there free courses available?",
   },
   {
+    answer: "Yes, you can access your courses on multiple devices.",
     question: "Can I access my courses on multiple devices?",
   },
   {
+    answer:
+      "No, there are no prerequisites for enrolling in the courses. However, they have been divided into different age based categories.",
     question: "Are there any prerequisites for enrolling in these courses??",
   },
   {
+    answer:
+      "Yes, you can interact with your trainer directly after the live training classes. You can also engage with your peers and network with them.",
     question: "Can I interact with my trainer and other members?",
   },
   {
+    answer:
+      "Yes, additional resources will be made available in offline mode that can easily be downloaded depending upon the course and the course structure.",
     question: "Are there additional resources available in the offline mode?",
   },
 ];
