f7d74ddd231d881f610d7ce38b354a3f2eb3dce3
div-vik <shivamambast5@gmail.com>
2024-02-04 02:37:31 +0530

    Create button component

---
+++ app/ui/Button.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Button = ({ title, className }) => {
+  return (
+    <button
+      className={
+        className +
+        " w-[159.152px] lg:w-[202.344px] xl:w-[202px] h-[52px] lg:h-[66.112px] xl:h-[66px] rounded-[33px] bg-gradient-to-r from-secondaryPink to-secondaryBlue hover:bg-gradient-to-r hover:from-[#000] hover:to-[#000] hover:text-[#fff] text-center font-bold lowercase"
+      }
+    >
+      {title}
+    </button>
+  );
+};
+
+export default Button;
