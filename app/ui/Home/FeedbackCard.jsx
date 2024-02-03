d42dca6292c7fcdba8cae70d63a4a2e5cc9ee4bf
div-vik <shivamambast5@gmail.com>
2024-02-04 05:17:24 +0530

    Update font

--- app/ui/Home/FeedbackCard.jsx
+++ app/ui/Home/FeedbackCard.jsx
@@ -6,14 +6,19 @@ const nature = localFont({
   display: "swap",
 });
 
+const zcool = localFont({
+  src: "../../../public/ZCOOL_XiaoWei_Regular_400.ttf",
+  display: "swap",
+});
+
 const FeedbackCard = ({ name, review, designation }) => {
   return (
     <div className="flex w-[218.667px] lg:w-[483.922px] xl:w-[483.92px] h-[182px] lg:h-[402.776px] xl:h-[402.78px] rounded-[27.112px] lg:rounded-[60px] bg-gradient-to-r from-ternaryPink to-primaryBlue relative">
-      <span className="text-[57.839px] lg:text-[128px] font-normal text-[#000] pl-5 lg:pl-[60px]">
-        &quot;
-      </span>
+      <div className="text-[57.839px] lg:text-[128px] font-normal text-[#000] pl-5 lg:pl-[60px]">
+        <span className={zcool.className}>&quot;</span>
+      </div>
 
-      <div className="flex flex-col justify-center items-center pt-5 pl-[5px] pr-[20px] lg:pr-[76px]">
+      <div className="flex flex-col justify-center items-center pt-20 pl-[5px] pr-[20px] lg:pr-[76px]">
         <div className="text-[9.037px] nature-beauty-personal text-left lg:text-base xl:text-[15px] font-normal leading-[16.719px] lg:leading-[37px] text-[#000]">
           <p className={nature.className}>{review}</p>
         </div>
