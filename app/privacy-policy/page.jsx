88ebb39b2eda11b5875bc4d97f847554ae507278
div-vik <shivamambast5@gmail.com>
2024-02-04 15:31:46 +0530

    Complete design

---
+++ app/privacy-policy/page.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="mt-[72.57px] lg:mt-[51.57px] xl:mt-[113px] px-[25px] lg:px-[54px] xl:px-[224px]">
+      <div>
+        <p className="text-center text-[22.638px] lg:text-[36.93px] xl:text-[40px] font-bold text-primaryPink">
+          Privacy Policy
+        </p>
+
+        <div className="mt-[40px] lg:mt-[64.94px] xl:mt-[70px] text-justify flex flex-col gap-[26px] lg:gap-[12.95px] xl:gap-[34px] text-[10.187px] lg:text-[16.619px] xl:text-[18px] font-medium tracking-[1.528px] lg:tracking-[2.493px] xl:tracking-[2.7px] leading-normal">
+          <p>
+            Vocademyy, a vocational online platform is committed to protecting
+            your privacy and ensuring a safe online experience. We try not to
+            collect or post personal information about individuals except when
+            you willingly register on our site, respond to a survey or fill out
+            a form. When registering on our site, as appropriate, you may be
+            asked to enter your personal details like name, email address,
+            mailing address, phone number or more.
+          </p>
+          <p>
+            We might collect your name, email ID, phone number, address, IP
+            address, payment and billing information. We never collect your
+            credit card number or credit card expiry date, CVV or other details
+            pertaining to your credit card on our website. This information is
+            not transferred or sold to any third party unless otherwise stated
+            or with the approval of the user. However, this does not include
+            trusted third parties who assist us with online operations. Personal
+            information might be released in compliance with laws or rules by
+            the Government. The information collected can be used to personalize
+            your experience, cater to your individual needs, improve our
+            collateral (online and offline), improve customer service, send
+            periodic emails, and respond to enquiries or other questions. Any
+            photograph, video or recording of any students will not be used
+            without good reason and without proper consent.
+          </p>
+          <p>
+            We use cookies to help us remember, understand and save your
+            preferences for future visits,and compile aggregate data about site
+            traffic and interaction so that we can ensure a better and
+            personalized site experience. You may refuse to accept cookies by
+            activating the setting on your browser.
+          </p>
+          <p>
+            We implement all possible security measures to maintain the safety
+            of your personal information. However, we are not responsible for
+            the authenticity of the personal information supplied to us by any
+            individual or company.
+          </p>
+        </div>
+      </div>
+
+      <div className="mt-[50px] lg:mt-[101.57px] xl:mt-[103px] flex flex-col">
+        <p className="text-center text-[22.638px] lg:text-[36.93px] xl:text-[40px] font-bold text-primaryPink">
+          Changes to this Privacy Policy
+        </p>
+
+        <div className="mt-[26px] lg:mt-[64.94px] xl:mt-[70px] flex flex-col text-justify gap-[26px] lg:gap-[34.19px] xl:gap-[34px] text-[10.187px] lg:text-[16.619px] xl:text-[18px] font-medium tracking-[1.528px] lg:tracking-[2.493px] xl:tracking-[2.7px] leading-normal">
+          <p>
+            Vocademyy reserves the right to make changes to the policies or its
+            content at any given point in time, without prior notice. The rights
+            to the content on this website and websites of related institutions
+            reside with Vocademy and are at its sole discretion. Any type of
+            content or visuals available on the websites cannot be used in any
+            form without permission from Vocademy. Any reproduction of any
+            information, with or without any modification is prohibited without
+            explicit written permission and shall amount to a violation of
+            copyright and would be treated as an illegal act. You are advised to
+            review this Privacy Policy periodically for any changes. Changes to
+            this Privacy Policy is effective when they are posted on this page.
+          </p>
+          <p>
+            This Privacy Policy was last updated on ___________ <br />
+            By using our site, you consent to our website&apos;s privacy policy.
+            If you have any questions about this Privacy Policy, You can contact
+            us via email at vocademy.co.in.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
