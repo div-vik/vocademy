8452032a652ea574002f81278f95ecafd338627e
div-vik <shivamambast5@gmail.com>
2024-02-04 19:40:20 +0530

    Update css

--- app/ui/Home/Faq.jsx
+++ app/ui/Home/Faq.jsx
@@ -4,7 +4,7 @@ import Accordian from "./Accordian";
 
 const Faq = () => {
   return (
-    <div className="mt-[45.96px] lg:mt-[102px] xl:mt-[181px] w-full flex flex-col justify-center items-center">
+    <div className="mt-[45.96px] lg:mt-[102px] xl:mt-[181px] mb-[35px] lg:mb-[102.47px] xl:mb-[59px] w-full flex flex-col justify-center items-center">
       <p className="text-center text-[36px] lg:text-5xl font-bold">FAQs</p>
 
       <div className="flex flex-col w-full px-[43px] lg:px-[62px] xl:px-[192px] mt-[46px] lg:mt-[54px] xl:mt-[24px]">
