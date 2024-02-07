4a2ed4a0c28230eb9f68777af53afefd1ec45ffc
div-vik <shivamambast5@gmail.com>
2024-02-07 17:23:51 +0530

    Complete design

--- app/careers/page.jsx
+++ app/careers/page.jsx
@@ -1,7 +1,92 @@
 import React from "react";
+import Button from "../ui/Button";
 
 const Careers = () => {
-  return <div>Careers</div>;
+  return (
+    <div className="mt-[84.57px] lg:mt-[185.57px] xl:mt-[161.57px] mb-[34.96px] lg:mb-[109px] xl:mb-[47px] flex flex-col justify-center items-center">
+      <div className="leading-tight flex flex-col justify-center items-center gap-[39px] lg:gap-[68px] xl:gap-[40px]">
+        <p className="w-[362px] lg:w-[905px] xl:w-[1151px] text-[34.12px] lg:text-[55.85px] xl:text-[64px] font-bold text-center">
+          We are always ready to partner up with ambitious and{" "}
+          <span className="bg-gradient-to-r from-secondaryPink from-20% to-secondaryBlue to-60% text-transparent bg-clip-text">
+            passionate{" "}
+          </span>
+          coaches. Join us in revolutionizing life skills as a trainer…
+        </p>
+        <p className="w-[291px] lg:w-[659px] xl:w-[828px] text-[12px] lg:text-[20.023px] xl:text-[24px] font-medium text-center">
+          Explore remote opportunities and join our mission to make vocational
+          skill training accessible and easier to help people grow.
+        </p>
+      </div>
+
+      <div className="leading-tight flex flex-col justify-center items-center mt-[29px] lg:mt-[42px] xl:mt-[90px] gap-[29px] lg:gap-[42px] xl:gap-[40px]">
+        <p className="w-[364px] lg:w-[761px] xl:w-[1016px] text-[32.479px] lg:text-[48px] xl:text-[75px] font-bold text-center">
+          Vocademyy is where the New Learning works
+        </p>
+        <p className="w-[282px] lg:w-[684px] xl:w-[866px] text-[12px] lg:text-[20px] font-medium text-center">
+          Every day we are refining and exploring ways to fill the skill gap to
+          help people upscale effectively. Join us in creating a better future
+          of work that unites, is diverse and flexible:
+        </p>
+      </div>
+
+      <div className="mt-[47px] lg:mt-[55px] xl:mt-[99px] gap-[6.1px] lg:gap-[14.36px] xl:gap-[22px] flex justify-center items-center">
+        <div className="w-[124.882px] h-[175px] lg:w-[293.819px] lg:h-[412px] xl:w-[450px] xl:h-[631px] gap-[19.31px] lg:gap-[45.79px] xl:gap-[70px] border-[1.39px] lg:border-[3.26px] xl:border-[5px] rounded-[24.1px] lg:rounded-[56.8px] xl:rounded-[87px] flex flex-col justify-center items-center border-quaternaryPink">
+          <div className="mt-[26.9px] lg:mt-[63.33px] xl:mt-[97px] relative flex justify-center items-center w-full">
+            <p className="text-[26.624px] lg:text-[62.681px] xl:text-[96px] font-bold z-10 text-center">
+              United
+            </p>
+            <div className="w-[26.624px] h-[26.624px] lg:w-[62.681px] lg:h-[62.681px] xl:w-[96px] xl:h-[96px] absolute right-[11.09px] lg:right-[26.12px] xl:right-[40px] bg-primaryBlue rounded-full" />
+          </div>
+
+          <div className="mb-[24.79px] lg:mb-[59.87px] xl:mb-[91px] w-[81.537px] lg:w-[195.88px] xl:w-[300px] text-[9.984px] lg:text-[23.506px] xl:text-[36px] text-center font-medium leading-tight">
+            <p>
+              We come together, no matter where you are- across the country and
+              beyond
+            </p>
+          </div>
+        </div>
+
+        <div className="w-[124.882px] h-[175px] lg:w-[293.819px] lg:h-[412px] xl:w-[450px] xl:h-[631px] gap-[19.31px] lg:gap-[45.79px] xl:gap-[70px] border-[1.39px] lg:border-[3.26px] xl:border-[5px] rounded-[24.1px] lg:rounded-[56.8px] xl:rounded-[87px] flex flex-col justify-center items-center border-quaternaryPink">
+          <div className="mt-[26.9px] lg:mt-[63.33px] xl:mt-[97px] relative flex justify-center items-center w-full">
+            <p className="text-[26.624px] lg:text-[62.681px] xl:text-[96px] font-bold z-10 text-center">
+              Diverse
+            </p>
+            <div className="w-[26.624px] h-[26.624px] lg:w-[62.681px] lg:h-[62.681px] xl:w-[96px] xl:h-[96px] absolute right-[11.09px] lg:right-[26.12px] xl:right-[40px] bg-primaryBlue rounded-full" />
+          </div>
+
+          <div className="mb-[24.79px] lg:mb-[59.87px] xl:mb-[91px] w-[83.201px] lg:w-[195.88px] xl:w-[300px] text-[9.984px] lg:text-[23.506px] xl:text-[36px] text-center font-medium leading-tight">
+            <p>
+              Our team brings rich diversity creating an inclusive environment
+              for everyone onboard.
+            </p>
+          </div>
+        </div>
+
+        <div className="w-[124.882px] h-[175px] lg:w-[293.819px] lg:h-[412px] xl:w-[450px] xl:h-[631px] gap-[19.31px] lg:gap-[45.79px] xl:gap-[70px] border-[1.39px] lg:border-[3.26px] xl:border-[5px] rounded-[24.1px] lg:rounded-[56.8px] xl:rounded-[87px] flex flex-col justify-center items-center border-quaternaryPink">
+          <div className="mt-[26.9px] lg:mt-[63.33px] xl:mt-[97px] relative flex justify-center items-center w-full">
+            <p className="text-[26.624px] lg:text-[62.681px] xl:text-[96px] font-bold z-10 text-center">
+              Flexible
+            </p>
+            <div className="w-[26.624px] h-[26.624px] lg:w-[62.681px] lg:h-[62.681px] xl:w-[96px] xl:h-[96px] absolute right-[11.09px] lg:right-[26.12px] xl:right-[40px] bg-primaryBlue rounded-full" />
+          </div>
+
+          <div className="mb-[12.79px] lg:mb-[31.87px] xl:mb-[48px] w-[83.201px] lg:w-[195.88px] xl:w-[300px] text-[9.984px] lg:text-[23.506px] xl:text-[36px] text-center font-medium leading-tight">
+            <p>
+              We strive for work-life balance to give you the freedom and
+              flexibility to deliver the best of our online sessions. 
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <Button
+          title="join now"
+          className="mt-[43px] lg:mt-[109px] xl:mt-[79px] text-[15.77px] lg:text-[24px] xl:text-[32px]"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Careers;
