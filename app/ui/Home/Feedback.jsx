8f65c3d5c738d9cdac954cdabb51126f9ac87204
div-vik <shivamambast5@gmail.com>
2024-02-13 23:17:32 +0530

    Update css

--- app/ui/Home/Feedback.jsx
+++ app/ui/Home/Feedback.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { feedbacks } from "../../constant/data";
 import FeedbackCard from "./FeedbackCard";
@@ -11,8 +13,8 @@ const Feedback = () => {
         </p>
       </div>
 
-      <div className="flex hover:animate-none">
-        <div className="animate-slide mt-[31px] lg:mt-[83px] xl:mt-[62px] px-[10px] flex justify-center xl:justify-normal items-center overflow-hidden xl:px-[52px] gap-[45.26px] lg:gap-[100.16px]">
+      <div className="flex justify-center items-center overflow-hidden w-full">
+        <div className="animate-slide mt-[31px] lg:mt-[83px] xl:mt-[62px] flex justify-between pr-[45.26px] lg:pr-[100.16px] gap-[45.26px] lg:gap-[100.16px] items-center">
           {feedbacks.map((feedback, index) => (
             <div key={index}>
               <FeedbackCard
@@ -23,7 +25,7 @@ const Feedback = () => {
             </div>
           ))}
         </div>
-        <div className="animate-slide mt-[31px] lg:mt-[83px] xl:mt-[62px] px-[10px] flex justify-center xl:justify-normal items-center overflow-hidden xl:overflow-x-scroll 2xl:overflow-hidden xl:px-[52px] gap-[45.26px] lg:gap-[100.16px]">
+        <div className="animate-slide mt-[31px] lg:mt-[83px] xl:mt-[62px] flex justify-between pr-[45.26px] lg:pr-[100.16px] gap-[45.26px] lg:gap-[100.16px] items-center">
           {feedbacks.map((feedback, index) => (
             <div key={index}>
               <FeedbackCard
@@ -34,7 +36,7 @@ const Feedback = () => {
             </div>
           ))}
         </div>
-        <div className="animate-slide mt-[31px] lg:mt-[83px] xl:mt-[62px] px-[10px] flex justify-center xl:justify-normal items-center overflow-hidden xl:overflow-x-scroll 2xl:overflow-hidden xl:px-[52px] gap-[45.26px] lg:gap-[100.16px]">
+        <div className="animate-slide mt-[31px] lg:mt-[83px] xl:mt-[62px] flex justify-between pr-[45.26px] lg:pr-[100.16px] gap-[45.26px] lg:gap-[100.16px] items-center">
           {feedbacks.map((feedback, index) => (
             <div key={index}>
               <FeedbackCard
@@ -45,7 +47,7 @@ const Feedback = () => {
             </div>
           ))}
         </div>
-        <div className="animate-slide mt-[31px] lg:mt-[83px] xl:mt-[62px] px-[10px] flex justify-center xl:justify-normal items-center overflow-hidden xl:overflow-x-scroll 2xl:overflow-hidden xl:px-[52px] gap-[45.26px] lg:gap-[100.16px]">
+        {/* <div className="animate-slide mt-[31px] lg:mt-[83px] xl:mt-[62px] px-[10px] hidden md:flex justify-center xl:justify-normal items-center overflow-hidden w-full xl:px-[52px] gap-[45.26px] lg:gap-[100.16px]">
           {feedbacks.map((feedback, index) => (
             <div key={index}>
               <FeedbackCard
@@ -56,6 +58,17 @@ const Feedback = () => {
             </div>
           ))}
         </div>
+        <div className="hidden xl:flex animate-slide mt-[31px] lg:mt-[83px] xl:mt-[62px] px-[10px] justify-center xl:justify-normal items-center overflow-hidden w-full xl:px-[52px] gap-[45.26px] lg:gap-[100.16px]">
+          {feedbacks.map((feedback, index) => (
+            <div key={index}>
+              <FeedbackCard
+                name={feedback.name}
+                review={feedback.review}
+                designation={feedback.designation}
+              />
+            </div>
+          ))}
+        </div> */}
       </div>
     </div>
   );
--- app/ui/Home/FeedbackCard.jsx
+++ app/ui/Home/FeedbackCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import localFont from "next/font/local";
+import Image from "next/image";
 
 const nature = localFont({
   src: "../../../public/Nature_Beauty_Personal.ttf",
@@ -14,15 +15,22 @@ const zcool = localFont({
 const FeedbackCard = ({ name, review, designation }) => {
   return (
     <div className="flex w-[218.667px] lg:w-[483.922px] xl:w-[483.92px] h-[182px] lg:h-[402.776px] xl:h-[402.78px] rounded-[27.112px] lg:rounded-[60px] bg-gradient-to-r from-ternaryPink to-primaryBlue relative">
-      <div className="w-fit h-fit flex justify-center items-start text-[57.839px] lg:text-[128px] font-normal text-[#000] pl-5 lg:pl-[60px]">
-        <span className={zcool.className}>&quot;</span>
+      <div className="w-fit h-fit inline-flex justify-center items-start text-[57.839px] lg:text-[128px] font-normal text-[#000] pl-5 lg:pt-5">
+        {/* <span className={zcool.className}>&quot;</span> */}
+        <Image
+          className="w-[25px] h-[58px] lg:w-[40px]"
+          src="/quot.svg"
+          alt="quot"
+          width={25}
+          height={58}
+        />
       </div>
 
-      <div className="flex flex-col justify-center items-center pl-[5px] pr-[20px] lg:pr-[76px]">
-        <div className="text-[9.037px] pt-16 xl:pt-20 text-left lg:text-base xl:text-[15px] font-normal leading-[16.719px] lg:leading-[37px] text-[#000]">
+      <div className="flex flex-col relative">
+        <div className="mt-[30px] lg:mt-[45px] pl-2 text-[7.23px] w-[144.596px] lg:w-[320px] xl:w-[341px] lg:text-base xl:text-[15px] text-justify font-normal leading-[16.719px] lg:leading-[37px] text-[#000]">
           <p className={nature.className}>{review}</p>
         </div>
-        <div className="w-full flex flex-col items-end text-left px-2 py-3 text-[#000] text-[7.23px] lg:text-[16px] font-extrabold leading-[9.941px] lg:leading-[22px]">
+        <div className="absolute bottom-0 lg:bottom-5 w-full flex flex-col items-end text-left px-2 py-3 text-[#000] text-[7.23px] lg:text-[16px] font-extrabold leading-[9.941px] lg:leading-[22px]">
           <p>{name}</p>
           <p>{designation}</p>
         </div>
